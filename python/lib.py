5ff93556d7c0f53cde099b1539efa739af79deae
Nathan Wycoff <nathanbrwycoff@gmail.com>
2020-01-14 13:12:34 -0500

    LIF SGD learning implemented

--- python/lib.py
+++ python/lib.py
@@ -12,54 +12,136 @@ class NeuralNetwork(object):
     g - State transition function
     get_xt - Input function
 
+    R - Dimension of state space for a single neuron.
     H - Number of recurrent neurons.
     P - Number of input channels.
     Q - Number of output channels.
 
+    net_params - Additional, fixed network parameters, such as membrane time constants. Should be a dictionary of arbitrary objects.
+    trainable_params - Parameters to be learned via eprop, such as connection weights. Should be a dictionary of np.arrays.
+    eprop_funcs - A dictionary of functions needed to do eprop.
+
     """
-    def __init__(self, f, g, get_xt, H, P, Q, params):
+
+    #TODO: learn rate should be a full optmier.
+    def __init__(self, f, g, get_xt, R, H, P, Q, net_params, trainable_params, eprop_funcs = None, learn_rate = 0.001):
         self.f = f
         self.g = g
         self.get_xt = get_xt
         self.H = H
         self.P = P
         self.Q = Q
-        self.params = params
+        self.R = R
+        self.net_params = net_params
+        self.trainable_params = trainable_params
+        self.eprop_funcs = eprop_funcs
+        self.learn_rate = learn_rate
+
+        assert Q == 1
 
         # Initialize state to zeros.
-        self.st1 = np.zeros([self.H])
+        self.st1 = np.zeros([self.R, self.H])
         self.zt1 = f(self, self.st1)
 
-    def run(self, t_steps, ip, save_states = False):
+    def run(self, t_steps, ip, target = None, train = False, save_states = False):
         """
         Run the simulation for the specified number of steps with input parameters given in the dictionary ip.
 
         if save_states is true, will return the values of the latent and observable states for all timesteps of the simulation.
+
+        target - An indexible collection of length t_steps; gives the target at a given time point (1D for now)
         """
+
+
+        if train:
+            if target is None:
+                raise ValueError("Need a target to do training; unsupervised training is not available.")
+            if self.eprop_funcs is None:
+                raise ValueError("Need to specify eprop_funcs to do training; audo-diff is not available.")
+
+        if train:
+            epst1 = np.zeros([self.R, self.H, self.H])
+            filt_et = np.zeros([self.H, self.H])
+            grad_rec = np.zeros([self.H, self.H])
+            grad_out = np.zeros([self.Q, self.H])
+            dydout = np.zeros([self.Q,self.H])
+
+        if target is not None:
+            cost = 0
 
         if save_states:
             S = np.zeros([self.H, t_steps])
             Z = np.zeros([self.H, t_steps])
 
+        ys = np.zeros([self.Q, t_steps])
+        yt1 = np.zeros([self.Q,1])
+
         st1 = self.st1
         zt1 = self.zt1
         for ti in tqdm(range(t_steps)):
+            #if ti == 20:
+            #    break
             xt = self.get_xt(self, ip, ti)
             st = self.g(self, st1, zt1, xt)
             zt = self.f(self, st)
 
-            st1 = st
-            zt1 = zt
+            yt = self.net_params['kappa'] * yt1 + self.trainable_params['out'] @ zt
+            ys[:,ti] = yt
+
+            if target is not None:
+                cost += np.sum(np.square(yt - target[ti]))
+
+            if train:
+                # E-prop 1 for Recurrent connectivity.
+                Dt = self.eprop_funcs['D'](self, st, st1)
+                dsdt = self.eprop_funcs['d_st_d_tp'](self, st, zt)
+                epst = Dt * (epst1 + dsdt)
+                h = self.eprop_funcs['h'](self, st, zt)
+                dzds = (h.T).reshape([self.H, self.R, 1])
+                et = (np.transpose(epst, [2,1,0]) @ dzds).reshape([self.H, self.H])
+
+                filt_et = kappa * filt_et + et
+                
+                Lt = self.eprop_funcs['L'](self, yt, target[ti])
+
+                gradt_rec = filt_et * Lt
+                grad_rec += gradt_rec
+
+                # Readout weights are easier.
+                assert self.Q == 1
+                delta = -float(target[ti] - yt) 
+                dydout = self.net_params['kappa'] * dydout + zt
+                gradt_out = delta * dydout
+                grad_out += gradt_out
 
             if save_states:
                 S[:,ti] = st
                 Z[:,ti] = zt
 
+            st1 = st
+            zt1 = zt
+            yt1 = yt
+
+        # Just gradient descent for now, and just on recurrent connections.
+        grad_rec = grad_rec - np.diag(np.diag(grad_rec))
+        self.trainable_params['rec'] = self.trainable_params['rec'] - self.learn_rate * grad_rec
+        self.trainable_params['out'] = self.trainable_params['out'] - self.learn_rate * grad_out
+
         self.st1 = st1
         self.zt1 = zt1
 
+        ret = {}
+        ret['y'] = ys
+
         if save_states:
-            return([S, Z])
+            ret['S'] = S
+            ret['Z'] = Z
+        if train:
+            ret['grad_rec'] = grad_rec
+        if target is not None:
+            ret['cost'] = cost
+
+        return ret
 
 
 class SpikingNeuralNetwork(NeuralNetwork):
--- python/test_eprop1_lif.py
+++ python/test_eprop1_lif.py
@@ -14,6 +14,7 @@ np.random.seed(123)
 H = 5
 P = 2
 Q = 1
+R = 1
 mult = 100. / np.sqrt(H)
 
 thresh = 1.0
@@ -21,15 +22,15 @@ t_eps = 0.01
 alpha = np.exp(-t_eps)
 kappa = np.exp(-t_eps)
 
-THETA_in = mult * np.random.normal(size=[H,P]) * t_eps
+THETA_in = mult * np.abs(np.random.normal(size=[H,P]) * t_eps)
 THETA_rec = mult * np.random.normal(size=[H,H]) * t_eps
 THETA_rec -= np.diag(np.diag(THETA_rec)) # No recurrent connectivity.
 THETA_out = np.random.normal(size=[Q,H])
 THETA_out_rand = np.random.normal(size=[Q,H]) #Random feedback weights, see how that works.
 
 t_steps = 2000
-ETA = 0.3 # Called gamma in the article
-LEARN_RATE = 1e-5
+gamma = 0.3 # Called gamma in the article
+learn_rate = 1e-6
 EPOCHS = 10
 
 # Make up some input spikes.
@@ -50,11 +51,34 @@ target = np.ones([t_steps]) - 2 * np.array(np.linspace(0,t_steps*t_eps,t_steps) 
 
 inlist = [spikes1, spikes2]
 
-nn_params = {'in' : THETA_in, 'rec' : THETA_rec, 'out' : THETA_out, 'thresh' : thresh, 'alpha' : alpha, 'kappa' : kappa, 't_eps' : t_eps}
+nn_params = {'thresh' : thresh, 'alpha' : alpha, 'kappa' : kappa, 't_eps' : t_eps, 'gamma' : gamma}
+trainable_params = {'in' : THETA_in, 'rec' : THETA_rec, 'out' : THETA_out}
 in_params = {'in_spikes' : in_spikes}
 
+def L(nn, yt, target):
+    return nn.trainable_params['out'] * (yt - target)
+
+# Give the jacobian of the state vector with respect to the trainable parameters.
+# TODO: Think harder about the timing for this: we have st1 here but st in lib.py
+def d_st_d_tp(nn, st1, zt1):
+    return np.tile(zt1, (nn.H,1)).reshape([1,nn.H,nn.H])
+
+# Gives the jacobian of the state vector at time t wrt the state vector at time t-1.
+# Should return a tensor of shape RxRxH, element i,j,k represents the derivative of neuron state dimension i at time t wrt neuron state dimension j at time t-1 for neuron k.
+def D(nn, st, st1):
+    return np.repeat(nn.net_params['alpha'], nn.H).reshape([1,1,nn.H])
+
+# This is an H by R matrix to be returned, each element tells us the pseudo-derivative of the observable state of neuron h wrt to dimension r of the hidden state.
+def h(nn, st, zt):
+    diff = 1. - np.abs((st - nn.net_params['thresh']) / nn.net_params['thresh'])
+    diff *= nn.net_params['gamma'] 
+    diff *= (diff > 0).astype(np.float)
+    return diff
+
+eprop_funcs = {'L' : L, 'd_st_d_tp' : d_st_d_tp, 'D' : D, 'h' : h}
+
 def f(nn, st):
-    zt = st > nn.params['thresh']
+    zt = st[0,:] > nn.net_params['thresh']
     return zt
 
 def g(nn, st1, zt1, xt):
@@ -68,10 +92,11 @@ def g(nn, st1, zt1, xt):
     st = alpha * st
 
     # Integrate incoming spikes
-    st += nn.params['rec'] @ zt1
+    #st += zt1 @ nn.trainable_params['rec'].T
+    st += nn.trainable_params['rec'] @ zt1
 
     # Integrate external stimuli
-    st += nn.params['in'] @ xt
+    st += nn.trainable_params['in'] @ xt
 
     ## Ensure nonnegativity
     #st *= st > 0
@@ -83,12 +108,23 @@ def get_xt(nn, ip, ti):
     xt[ip['in_spikes'][ti]] = 1
     return xt
 
-snn = NeuralNetwork(f = f, g = g, get_xt = get_xt, H = H, P = P, Q = Q, params = nn_params)
+snn = NeuralNetwork(f = f, g = g, get_xt = get_xt, R = R, H = H, P = P, Q = Q, net_params = nn_params, trainable_params = trainable_params, eprop_funcs = eprop_funcs, learn_rate = learn_rate)
 
-S, Z = snn.run(t_steps = t_steps, ip = in_params, save_states = True)
+costs = np.empty([EPOCHS])
+for epoch in range(EPOCHS):
+    ret = snn.run(t_steps = t_steps, ip = in_params, target = target, train = True, save_states = True)
+    costs[epoch] = ret['cost']
+
+y = ret['y']
+S = ret['S']
 
 fig = plt.figure()
+plt.subplot(3,1,1)
 plt.plot(S.T)
+plt.subplot(3,1,2)
+plt.plot(y.flatten())
+plt.subplot(3,1,3)
+plt.plot(costs)
 plt.savefig("temp.pdf")
 plt.close()
 
