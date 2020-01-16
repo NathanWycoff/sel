1be6c537033065a3957ce402839f0ac3648edbf6
Nathan Wycoff <nathanbrwycoff@gmail.com>
2020-01-16 12:38:47 -0500

    Implemented ALIF functions, which resulted in many EPROP bugfixes

--- python/lib.py
+++ python/lib.py
@@ -2,8 +2,6 @@
 # -*- coding: utf-8 -*-
 #  python/lib.py Author "Nathan Wycoff <nathanbrwycoff@gmail.com>" Date 01.12.2020
 
-from tqdm import tqdm
-
 class NeuralNetwork(object):
     """
     A very general neural network as defined in Bellec et al. 
@@ -60,8 +58,11 @@ class NeuralNetwork(object):
                 raise ValueError("Need to specify eprop_funcs to do training; audo-diff is not available.")
 
         if train:
-            epst1 = np.zeros([self.R, self.H, self.H])
+            epst1 = np.zeros([self.H, self.H, self.R, 1])
+            epst1_in = np.zeros([self.H, self.P, self.R, 1])
             filt_et = np.zeros([self.H, self.H])
+            filt_et_in = np.zeros([self.H, self.P])
+            grad_in = np.zeros([self.H, self.P])
             grad_rec = np.zeros([self.H, self.H])
             grad_out = np.zeros([self.Q, self.H])
             dydout = np.zeros([self.Q,self.H])
@@ -70,7 +71,7 @@ class NeuralNetwork(object):
             cost = 0
 
         if save_states:
-            S = np.zeros([self.H, t_steps])
+            S = np.zeros([self.R, self.H, t_steps])
             Z = np.zeros([self.H, t_steps])
 
         ys = np.zeros([self.Q, t_steps])
@@ -78,9 +79,7 @@ class NeuralNetwork(object):
 
         st1 = self.st1
         zt1 = self.zt1
-        for ti in tqdm(range(t_steps)):
-            #if ti == 20:
-            #    break
+        for ti in range(t_steps):
             xt = self.get_xt(self, ip, ti)
             st = self.g(self, st1, zt1, xt)
             zt = self.f(self, st)
@@ -92,30 +91,66 @@ class NeuralNetwork(object):
                 cost += np.sum(np.square(yt - target[ti]))
 
             if train:
-                # E-prop 1 for Recurrent connectivity.
+
+                # TODO: There is much code reuse between these next few blocks: we should write a function. The logic is very similar as it is, but I think there's a smart way to simultaneously get rid of tiling and make the code more similar.
+
+                ## E-prop 1 for input connectivity.
+                #TODO: The tiling is highly inefficient, I'm sure we can figure out how to do it without it.
                 Dt = self.eprop_funcs['D'](self, st, st1)
+                Dts = np.expand_dims(Dt, 1)
+                Dts = np.tile(Dts, (1,self.P,1,1))
+                dsdt = self.eprop_funcs['d_st_d_tp'](self, st, xt)
+                epst_in = Dts @ epst1_in + dsdt
+                dzds = self.eprop_funcs['dzds'](self, st)
+                dzds = np.expand_dims(dzds, 1)
+                dzds = np.expand_dims(dzds, 1)
+                dzds = np.tile(dzds, (1,self.P,1,1))
+                et_in = (dzds @ epst_in).reshape([self.H,self.P])
+
+                filt_et_in = kappa * filt_et_in + et_in
+                
+                Lt = self.eprop_funcs['L'](self, yt, target[ti])
+
+                # The commented out line seems to be equivalent (and hence, more efficient), although less readable, as array multiplication with different dimensions is unintuitive.
+                #gradt_in = filt_et_in * Lt
+                gradt_in = np.diag(Lt) @ filt_et_in
+                grad_in += gradt_in
+
+                # E-prop 1 for Recurrent connectivity.
+                #TODO: The tiling is highly inefficient, I'm sure we can figure out how to do it without it.
+                Dt = self.eprop_funcs['D'](self, st, st1)
+                Dts = np.expand_dims(Dt, 1)
+                Dts = np.tile(Dts, (1,self.H,1,1))
                 dsdt = self.eprop_funcs['d_st_d_tp'](self, st, zt)
-                epst = Dt * (epst1 + dsdt)
-                h = self.eprop_funcs['h'](self, st, zt)
-                dzds = (h.T).reshape([self.H, self.R, 1])
-                et = (np.transpose(epst, [2,1,0]) @ dzds).reshape([self.H, self.H])
+                epst = Dts @ epst1 + dsdt
+                dzds = self.eprop_funcs['dzds'](self, st)
+                dzds = np.expand_dims(dzds, 1)
+                dzds = np.expand_dims(dzds, 1)
+                dzds = np.tile(dzds, (1,self.H,1,1))
+                et = (dzds @ epst).reshape([self.H,self.H])
 
                 filt_et = kappa * filt_et + et
                 
                 Lt = self.eprop_funcs['L'](self, yt, target[ti])
 
-                gradt_rec = filt_et * Lt
+                # The commented out line seems to be equivalent (and hence, more efficient)
+                #gradt_rec = filt_et * Lt
+                gradt_rec = np.diag(Lt) @ filt_et
                 grad_rec += gradt_rec
 
                 # Readout weights are easier.
                 assert self.Q == 1
-                delta = -float(target[ti] - yt) 
+                delta = float(yt - target[ti]) 
                 dydout = self.net_params['kappa'] * dydout + zt
                 gradt_out = delta * dydout
                 grad_out += gradt_out
 
+                # Update vars
+                epst1_in = epst_in
+                epst1 = epst
+
             if save_states:
-                S[:,ti] = st
+                S[:,:,ti] = st
                 Z[:,ti] = zt
 
             st1 = st
@@ -123,9 +158,12 @@ class NeuralNetwork(object):
             yt1 = yt
 
         # Just gradient descent for now, and just on recurrent connections.
-        grad_rec = grad_rec - np.diag(np.diag(grad_rec))
-        self.trainable_params['rec'] = self.trainable_params['rec'] - self.learn_rate * grad_rec
-        self.trainable_params['out'] = self.trainable_params['out'] - self.learn_rate * grad_out
+        if train:
+            # We choose to disallow self-connections, though it might be interesting to explore in the future.
+            grad_rec = grad_rec - np.diag(np.diag(grad_rec))
+            self.trainable_params['in'] = self.trainable_params['in'] - self.learn_rate * grad_in
+            self.trainable_params['rec'] = self.trainable_params['rec'] - self.learn_rate * grad_rec
+            self.trainable_params['out'] = self.trainable_params['out'] - self.learn_rate * grad_out
 
         self.st1 = st1
         self.zt1 = zt1
--- python/test_eprop1_alif.py
+++ python/test_eprop1_alif.py
@@ -9,18 +9,23 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 exec(open("python/lib.py").read())
 
-np.random.seed(123)
+np.random.seed(1234)
 
 H = 5
 P = 2
 Q = 1
-R = 1
+R = 2
 mult = 100. / np.sqrt(H)
 
 thresh = 1.0
 t_eps = 0.01
-alpha = np.exp(-t_eps)
-kappa = np.exp(-t_eps)
+tau_m = 0.2
+tau_o = tau_m
+tau_a = tau_m
+alpha = np.exp(-t_eps/tau_m)
+kappa = np.exp(-t_eps/tau_o)
+rho = np.exp(-t_eps/tau_a)
+beta = 0.03
 
 THETA_in = mult * np.abs(np.random.normal(size=[H,P]) * t_eps)
 THETA_rec = mult * np.random.normal(size=[H,H]) * t_eps
@@ -29,7 +34,7 @@ THETA_out = np.random.normal(size=[Q,H])
 
 t_steps = 2000
 gamma = 0.3 # Called gamma in the article
-learn_rate = 1e-6
+learn_rate = 5e-6
 EPOCHS = 20
 
 # Make up some input spikes.
@@ -50,37 +55,56 @@ target = np.ones([t_steps]) - 2 * np.array(np.linspace(0,t_steps*t_eps,t_steps) 
 
 inlist = [spikes1, spikes2]
 
-nn_params = {'thresh' : thresh, 'alpha' : alpha, 'kappa' : kappa, 't_eps' : t_eps, 'gamma' : gamma}
+nn_params = {'thresh' : thresh, 'alpha' : alpha, 'kappa' : kappa, 'beta' : beta,  'rho' : rho,  't_eps' : t_eps, 'gamma' : gamma}
 trainable_params = {'in' : THETA_in, 'rec' : THETA_rec, 'out' : THETA_out}
 in_params = {'in_spikes' : in_spikes}
 
 def L(nn, yt, target):
-    return nn.trainable_params['out'] * (yt - target)
+    return (nn.trainable_params['out'] * (yt - target)).flatten()
 
 # Give the jacobian of the state vector with respect to the trainable parameters.
 # TODO: Think harder about the timing for this: we have st1 here but st in lib.py
 # TODO: Think harder about naming the third param: it will sometimes be input spikes.
+# First index - to neuron; second index - from neuron
 def d_st_d_tp(nn, st1, zt1):
-    ret = np.tile(zt1, (nn.H,1))
-    ret = np.expand_dims(ret, 0)
+    wrt1 = np.tile(zt1, (nn.H,1))
+    wrt2 = np.zeros_like(wrt1)
+    ret = np.stack([wrt1, wrt2]).astype(np.float)
+    ret = np.transpose(ret, [1,2,0])
+    ret = np.expand_dims(ret, -1)
     return ret
 
 # Gives the jacobian of the state vector at time t wrt the state vector at time t-1.
 # Should return a tensor of shape RxRxH, element i,j,k represents the derivative of neuron state dimension i at time t wrt neuron state dimension j at time t-1 for neuron k.
 def D(nn, st, st1):
-    return np.repeat(nn.net_params['alpha'], nn.H).reshape([1,1,nn.H])
+    hs = 1. - np.abs((st[0,:] - nn.net_params['thresh']) / nn.net_params['thresh'])
+    hs *= nn.net_params['gamma'] 
+    hs *= (hs > 0).astype(np.float)
+
+    diag1 = np.repeat(nn.net_params['alpha'], nn.H)
+    diag2 = nn.net_params['rho'] - hs * nn.net_params['beta']
+    superdiag = np.zeros([nn.H])
+    subdiag = hs
+
+    ret = np.stack([diag1, superdiag, subdiag, diag2], axis = -1).reshape([nn.H,2,2])
+    return ret
 
 # This is an H by R matrix to be returned, each element tells us the pseudo-derivative of the observable state of neuron h wrt to dimension r of the hidden state.
-def h(nn, st, zt):
-    diff = 1. - np.abs((st - nn.net_params['thresh']) / nn.net_params['thresh'])
-    diff *= nn.net_params['gamma'] 
-    diff *= (diff > 0).astype(np.float)
-    return diff
+def dzds(nn, st):
+    state1 = 1. - np.abs((st[0,:] - nn.net_params['thresh']) / nn.net_params['thresh'])
+    state1 *= nn.net_params['gamma'] 
+    state1 *= (state1 > 0).astype(np.float)
+    state2 = (-1) * nn.net_params['beta'] * state1.copy()
+    ret = np.stack([state1, state2])
+    ret = ret.T
+    return ret
 
-eprop_funcs = {'L' : L, 'd_st_d_tp' : d_st_d_tp, 'D' : D, 'h' : h}
+eprop_funcs = {'L' : L, 'd_st_d_tp' : d_st_d_tp, 'D' : D, 'dzds' : dzds}
 
 def f(nn, st):
-    zt = st[0,:] > nn.net_params['thresh']
+    beta = nn.net_params['beta']
+    thresh = nn.net_params['thresh']
+    zt = (st[0,:] - beta * st[1,:]) > thresh
     return zt
 
 def g(nn, st1, zt1, xt):
@@ -91,14 +115,17 @@ def g(nn, st1, zt1, xt):
     st *= (1-zt1)
 
     # Decay Potential.
-    st = alpha * st
+    st[0,:] = alpha * st[0,:]
+
+    # Update adaptive excess threshold
+    st[1,:] = rho * st[1,:] + zt1
 
     # Integrate incoming spikes
     #st += zt1 @ nn.trainable_params['rec'].T
-    st += nn.trainable_params['rec'] @ zt1
+    st[0,:] += nn.trainable_params['rec'] @ zt1
 
     # Integrate external stimuli
-    st += nn.trainable_params['in'] @ xt
+    st[0,:] += nn.trainable_params['in'] @ xt
 
     ## Ensure nonnegativity
     #st *= st > 0
@@ -112,6 +139,8 @@ def get_xt(nn, ip, ti):
 
 snn = NeuralNetwork(f = f, g = g, get_xt = get_xt, R = R, H = H, P = P, Q = Q, net_params = nn_params, trainable_params = trainable_params, eprop_funcs = eprop_funcs, learn_rate = learn_rate)
 
+ret = snn.run(t_steps = t_steps, ip = in_params, target = None, train = False, save_states = True)
+
 costs = np.empty([EPOCHS])
 for epoch in tqdm(range(EPOCHS)):
     ret = snn.run(t_steps = t_steps, ip = in_params, target = target, train = True, save_states = True)
@@ -120,12 +149,18 @@ for epoch in tqdm(range(EPOCHS)):
 y = ret['y']
 S = ret['S']
 
-fig = plt.figure()
-plt.subplot(3,1,1)
-plt.plot(S.T)
-plt.subplot(3,1,2)
+fig = plt.figure(figsize=[8,8])
+plt.subplot(2,2,1)
+plt.plot(S[0,:,:].T)
+plt.title("Potential")
+plt.subplot(2,2,2)
+plt.plot(S[1,:,:].T)
+plt.title("Adaptive Threshold")
+plt.subplot(2,2,3)
 plt.plot(y.flatten())
-plt.subplot(3,1,3)
+plt.title("Output")
+plt.subplot(2,2,4)
 plt.plot(np.log10(costs))
+plt.title("Costs")
 plt.savefig("temp.pdf")
 plt.close()
