717254b26da6778b202d964e1f0731da15aa526b
Nathan Wycoff <nathanbrwycoff@gmail.com>
2020-02-10 12:55:28 -0500

    Updated ALIF toy

--- python/test_eprop1_alif.py
+++ python/test_eprop1_alif.py
@@ -15,27 +15,34 @@ H = 5
 P = 3
 Q = 1
 R = 2
-mult = 100. / np.sqrt(H)
+
+t_eps = 0.01
+t_end = 20
+t_steps = int(np.ceil(t_end/t_eps))
 
 thresh = 1.0
-t_eps = 0.01
+refractory_period = 0.05
+#refractory_period = 0.2
+refractory_steps = int(np.ceil(refractory_period / t_eps))
 tau_m = 0.2
 tau_o = tau_m
-tau_a = tau_m
+tau_a = 2.
 alpha = np.exp(-t_eps/tau_m)
 kappa = np.exp(-t_eps/tau_o)
 rho = np.exp(-t_eps/tau_a)
-beta = 0.03
+#betas = np.array([1.74e-2 if i < H/2 else 0. for i in range(H)])
+#betas = np.array([1. if i < H/2 else 0. for i in range(H)])
+betas = np.array([0.03 for i in range(H)])
 
-THETA_in = mult * np.abs(np.random.normal(size=[H,P]) * t_eps)
-THETA_rec = mult * np.random.normal(size=[H,H]) * t_eps
-THETA_rec -= np.diag(np.diag(THETA_rec)) # No recurrent connectivity.
-THETA_out = np.random.normal(size=[Q,H])
-
-t_steps = 2000
-gamma = 0.3 # Called gamma in the article
-learn_rate = 1e-1
-epochs = 10
+#THETA_in = np.array([[1., 0., 0., 0.], [0., 1.0, 0., 0.0], [0, 0, 0, 1], [0, 0, 0, 1]])
+mult = 0.1
+#THETA_in = mult * np.array([[1., 0., 0., 1.1/mult],[0., 1., 0., 1.1/mult]])
+isig = 1.0
+THETA_in = np.zeros([H,P]) + np.random.normal(size=[H,P], scale = isig)
+THETA_rec = np.zeros([H,H]) + np.random.normal(size=[H,H], scale = 0.001)
+THETA_out = np.zeros([Q,H]) + np.random.normal(size=[Q,H], scale = isig)
+#THETA_rec = np.zeros([H,H])
+#THETA_out = np.array([-1, 1.1]).reshape([Q, H])
 
 # Make up some input spikes.
 seq = np.linspace(t_eps * 10, t_eps * t_steps, t_steps / 10)
@@ -58,9 +65,13 @@ for spike in spikes3:
 #target = np.ones([t_steps]) - 2 * np.array(np.floor(np.linspace(0,t_steps*t_eps, t_steps)) % 2 == 0).astype(np.float64)
 target = np.ones([t_steps]) - 2 * np.array(np.linspace(0,t_steps*t_eps,t_steps) <= 10).astype(np.float64)
 
-nn_params = {'thresh' : thresh, 'alpha' : alpha, 'kappa' : kappa, 'beta' : beta,  'rho' : rho,  't_eps' : t_eps, 'gamma' : gamma}
+gamma = 0.3 # Called gamma in the article
+learn_rate = 0.1
+epochs = 100
+
+nn_params = {'thresh' : thresh, 'alpha' : alpha, 'kappa' : kappa, 'betas' : betas,  'rho' : rho,  't_eps' : t_eps, 'gamma' : gamma, 'spikes' : [], 'ref_steps' : refractory_steps, \
+        'rc' : np.zeros(H).astype(np.int)}
 trainable_params = {'in' : THETA_in, 'rec' : THETA_rec, 'out' : THETA_out}
-in_params = {'in_spikes' : in_spikes}
 
 def dEdy(nn, yt, target_t):
 
@@ -79,7 +90,9 @@ def L(nn, yt, target_t):
 # First index - to neuron; second index - from neuron
 def d_st_d_tp(nn, st1, zt1):
     wrt1 = np.tile(zt1, (nn.H,1))
-    wrt2 = np.zeros_like(wrt1)
+    wrt2 = np.zeros_like(wrt1) #TODO: Is this really zero?
+    #dzdv = nn.eprop_funcs['dzds'](nn, st1, zt1)[:,0]
+    #wrt2 = np.diag(dzdv) @ wrt1.copy()
     ret = np.stack([wrt1, wrt2]).astype(np.float)
     ret = np.transpose(ret, [1,2,0])
     ret = np.expand_dims(ret, -1)
@@ -88,12 +101,13 @@ def d_st_d_tp(nn, st1, zt1):
 # Gives the jacobian of the state vector at time t wrt the state vector at time t-1.
 # Should return a tensor of shape RxRxH, element i,j,k represents the derivative of neuron state dimension i at time t wrt neuron state dimension j at time t-1 for neuron k.
 def D(nn, st, st1):
-    hs = 1. - np.abs((st[0,:] - nn.net_params['thresh']) / nn.net_params['thresh'])
+    hs = (1. - np.abs(st1[0,:] - nn.net_params['thresh']) / nn.net_params['thresh'])
     hs *= nn.net_params['gamma'] 
     hs *= (hs > 0).astype(np.float)
 
+    # Perhaps it should be the case that this matrix should only have a nonzero 1,1 entry if beta is 0? Or perhaps not?
     diag1 = np.repeat(nn.net_params['alpha'], nn.H)
-    diag2 = nn.net_params['rho'] - hs * nn.net_params['beta']
+    diag2 = nn.net_params['rho'] - hs * nn.net_params['betas']
     superdiag = np.zeros([nn.H])
     subdiag = hs
 
@@ -101,29 +115,37 @@ def D(nn, st, st1):
     return ret
 
 # This is an H by R matrix to be returned, each element tells us the pseudo-derivative of the observable state of neuron h wrt to dimension r of the hidden state.
-def dzds(nn, st):
-    state1 = 1. - np.abs((st[0,:] - nn.net_params['thresh']) / nn.net_params['thresh'])
+def dzds(nn, st, zt):
+    state1 = (1. - np.abs(st[0,:] - nn.net_params['thresh'])) / nn.net_params['thresh']
     state1 *= nn.net_params['gamma'] 
     state1 *= (state1 > 0).astype(np.float)
-    state2 = (-1) * nn.net_params['beta'] * state1.copy()
+    state2 = (-1) * state1.copy() * nn.net_params['betas']
     ret = np.stack([state1, state2])
     ret = ret.T
     return ret
 
-eprop_funcs = {'L' : L, 'd_st_d_tp' : d_st_d_tp, 'D' : D, 'dzds' : dzds, "dEdy" : dEdy}
+eprop_funcs = {'L' : L, 'd_st_d_tp' : d_st_d_tp, 'D' : D, 'dzds' : dzds, 'dEdy' : dEdy}
 
 def f(nn, st):
-    beta = nn.net_params['beta']
+    betas = nn.net_params['betas']
     thresh = nn.net_params['thresh']
-    zt = (st[0,:] - beta * st[1,:]) > thresh
+    zt = (st[0,:] - betas * st[1,:]) > thresh
+    our_spikes = list(np.where(zt)[0])
+    nn.net_params['spikes'].append(our_spikes)
     return zt
 
 def g(nn, st1, zt1, xt):
+    # Lower refractory counters (some may go strongly negative, but this shouldn't be a problem).
+    nn.net_params['rc'] -= 1
+
     # Initialize at previous potential.
     st = np.copy(st1)
 
     # Reset neurons that spiked.
     st *= (1-zt1)
+
+    # Update refractory counter.
+    nn.net_params['rc'][zt1.astype(np.bool)] = nn.net_params['ref_steps']
 
     # Decay Potential.
     st[0,:] = alpha * st[0,:]
@@ -141,6 +163,10 @@ def g(nn, st1, zt1, xt):
     ## Ensure nonnegativity
     #st *= st > 0
 
+    # Reset any neurons still in their refractory period.
+    isref = (nn.net_params['rc'] > 0)
+    st[0,:] *= (1. - isref)
+
     return st
 
 def get_xt(nn, ip, ti):
@@ -148,14 +174,21 @@ def get_xt(nn, ip, ti):
     xt[ip['in_spikes'][ti]] = 1
     return xt
 
+
 opt = Adam(learn_rate = learn_rate)
-snn = NeuralNetwork(f = f, g = g, get_xt = get_xt, R = R, H = H, P = P, Q = Q, net_params = nn_params, trainable_params = trainable_params, optimizer = opt, eprop_funcs = eprop_funcs)
+#opt = GD(learn_rate = learn_rate)
+snn = NeuralNetwork(f = f, g = g, get_xt = get_xt, R = R, H = H, P = P, Q = Q, net_params = nn_params, trainable_params = trainable_params, optimizer = opt, eprop_funcs = eprop_funcs, costfunc = mse)
 
-ret = snn.run(t_steps = t_steps, ip = in_params, target = None, train = False, save_states = True)
+costs = np.zeros(epochs)
 
-costs = np.empty([epochs])
 for epoch in tqdm(range(epochs)):
-    ret = snn.run(t_steps = t_steps, ip = in_params, target = target, train = True, save_states = True)
+    in_params = {'in_spikes' : in_spikes}
+
+    #TODO: Betterrrr
+    snn.net_params['spikes'] = []
+    ret = snn.run(t_steps = t_steps, ip = in_params, target = target, train = True, save_states = True, save_traces = True)
+    snn.reset_states()
+
     costs[epoch] = ret['cost']
 
 y = ret['y']
